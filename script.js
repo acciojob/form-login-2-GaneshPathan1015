a23188213078c59622da79b79cc38f2c123b5bee
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2024-01-08 21:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const name = document.querySelector("#name")
+const lname = document.querySelector("#lname")
+const pnumber = document.querySelector("#pnumber")
+const email = document.querySelector("#email")
+
+const form = document.querySelector("form")
+
+form.addEventListener("submit",alt)
+function alt(){
+	alert(`First Name : ${name.value} Last Name : ${pnumber.value} `)
+}
